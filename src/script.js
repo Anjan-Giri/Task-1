9e4b7f7b703426c07502bdcad7fbbfc0478c7544
unknown <ahnjhan96@gmail.com>
2024-08-29 22:37:22 +0545

    day4

--- src/script.js
+++ src/script.js
@@ -1,32 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Swiper(".courses-grid", {
-    loop: true,
-    spaceBetween: 30,
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
 
-    // Pagnitation bullet
-    pagination: {
-      el: ".swiper-pagination",
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-      clickable: true,
-      dynamicBullets: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2,
     },
-
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    1024: {
+      slidesPerView: 3,
     },
-  });
+  },
 });
