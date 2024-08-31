71b8e4734787f4807609c7db77154344de8316f5
unknown <ahnjhan96@gmail.com>
2024-08-31 16:54:32 +0545

    some issue

--- src/script.js
+++ src/script.js
@@ -29,3 +29,44 @@ const swiper = new Swiper(".slider-wrapper", {
     },
   },
 });
+
+var swiperr = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperrr = new Swiper(".slider-wrapper", {
+  // centeredSlides: true,
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
